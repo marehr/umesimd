e1fe1c64fc5ea769d8139a157c548725c573a547
marehr <marcel.ehrhardt@fu-berlin.de>
2016-10-10 13:59:53 +0200

    [WIP] Example: missing constructor

--- examples/ScalarConstants.cpp
+++ examples/ScalarConstants.cpp
@@ -1,8 +1,8 @@
 #include "../UMESimd.h"
 
-// When developing a templated code, it is often easier to use 
+// When developing a templated code, it is often easier to use
 // scalar constants. The problem with scalar constants is, that there
-// are multiple representations of constant literals, and each of them is 
+// are multiple representations of constant literals, and each of them is
 // interpreted differently by the compiler, for example:
 //
 //  '1' - is interpreted as 'int'
@@ -40,7 +40,7 @@ void printVector(VEC_T const & x) {
     // If a certain type is not used in prototype of the template function,
     // it is not necessary to pass it using template type-list.
     // Instead, it is possible to get specific type from a helper traits
-    // class 'SIMDTraits' to be used in function scope. The SIMDTraits class defines 
+    // class 'SIMDTraits' to be used in function scope. The SIMDTraits class defines
     // typedefs for types that might be helpful when operating on a particular vector type.
     typedef typename UME::SIMD::SIMDTraits<VEC_T>::SCALAR_T SCALAR_T;
 
@@ -71,6 +71,18 @@ int main()
     v1 = offsetInit<UME::SIMD::SIMD4_64f, double>(3.2);
     std::cout << "SIMD4_64f after:\n";
     printVector(v1);
+
+UME::SIMD::SIMD32_8i v2(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+           16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31);
+std::cout << "SIMD32_8u:\n";
+printVector(v2);
+
+UME::SIMD::SIMD64_8i v3(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+           16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+           32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+           48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63);
+std::cout << "SIMD64_8u:\n";
+printVector(v3);
 
     return 0;
 }
